8feca54c38043875eed623172bf6296a4bb2f071
andy6804tw <andy6804tw@yahoo.com.tw>
2017-03-08 12:46:09 +0800

    The first commit RainingImageView

---
+++ app/src/main/java/com/openweather/rainingimageview_lib/MainActivity.java
@@ -0,0 +1,15 @@
+package com.openweather.rainingimageview_lib;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
